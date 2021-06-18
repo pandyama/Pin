d3f427a93ceeddd7cb39e6462df7f2b8e7850e1d
Meet Pandya <pandyama2@gmail.com>
2021-06-17 20:51:24 -0400

    Added keyPress Event Listener and DeletePin Listener

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-function numPress(num){
-    console.log(num);
-    document.getElementById("pin1").innerHTML = num;
+var counter = 0;
+
+function numPress(num) {
+  if (counter < 4) {
+    console.log("Pressed key"+num);
+    console.log("counter"+counter);
+    console.log("IF");
+    switch (counter) {
+      case 0:
+        document.getElementById("pin1").innerHTML = num;
+        break;
+      case 1:
+        document.getElementById("pin2").innerHTML = num;
+        break;
+      case 2:
+        document.getElementById("pin3").innerHTML = num;
+        break;
+      case 3:
+        document.getElementById("pin4").innerHTML = num;
+        break;
+
+    }
+    counter++;
+  } else {
+    console.log("4 Pins entered");
+  }
+}
+
+function deletePin(){
+    document.getElementById("pin1").innerHTML = "";
+    document.getElementById("pin2").innerHTML = "";
+    document.getElementById("pin3").innerHTML = "";
+    document.getElementById("pin4").innerHTML = "";
 }
